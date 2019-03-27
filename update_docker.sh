f08c3928ee16eb9586765014c472ee74779050df
voravitl <voravitl@hotmail.com>
2019-03-27 02:20:45 -0700

    sync

--- update_docker.sh
+++ update_docker.sh
@@ -1,4 +1,4 @@
 docker pull deviavir/zenbot:unstable
-docker-compose up -d --force-recreate server  -f /opt/zenbot/docker-compose.yaml
+docker-compose -f /opt/zenbot/docker-compose.yml up -d --force-recreate server 
 docker exec -it zenbot_server_1 apk add tzdata
 #docker commit zenbot_server_1 voravitl/zenbot:unstable
