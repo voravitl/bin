12bc3856bcdd964ca926d8117a223631a25bdf05
root <root@kook.in.th>
2020-02-28 08:22:56 +0700

    update

--- check_trade.sh
+++ check_trade.sh
@@ -4,8 +4,8 @@ COIN=$1
 
 LINE ()  {  #### $1
 BALANCE=$1
-#TOKEN="VuF3KSuL3cJuBxKgSO4gCe9lrnZFobt74g7IAr8vI48"
-TOKEN="9YOXNNLHwH4PnnzMlMnLEvMyBpT3uRt2cViqPdKIEyb"
+TOKEN="SxWi3636MoV2lDSBQ4n8L2N79RYa9q6NlnTivy562Dh"
+
 curl -X POST -H "Authorization: Bearer $TOKEN" -F 'message='"$BALANCE"' ' https://notify-api.line.me/api/notify
 }
 
@@ -35,7 +35,9 @@ cp  $last_error $provide_error
 chk_error2
 START=/tmp/bot_run.$COIN-start.txt
 END=/tmp/bot_run.$COIN-end.txt
-docker logs $COIN | grep $COIN-BTC | grep -v wrote  | awk '{print $1,$2}' | sed -e  's/\x1B\[\([0-9]\{1,2\}\(;[0-9]\{1,2\}\)\?\)\?[mGK]//g' | tail -1 > $START
+NAME_COIN=`echo $COIN | sed 's/[0-9]*//g'`
+
+docker logs $COIN | grep ${NAME_COIN}-BTC | grep -v wrote  | awk '{print $1,$2}' | sed -e  's/\x1B\[\([0-9]\{1,2\}\(;[0-9]\{1,2\}\)\?\)\?[mGK]//g' | tail -1 > $START
 if [ "`diff $START $END | wc -l `" -eq 0  ] ; then 
 diff $START $END
 LINE "BOT STOP TRADE . AUTO RESTART $COIN"
